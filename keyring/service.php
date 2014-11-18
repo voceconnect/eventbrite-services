887183f19cc3a8516f4432be7f34e1876012f23e
Sean McCafferty <smccafferty@voceconnect.com>
2014-11-18 10:24:07 -0500

    Adjusting formatting.

--- keyring/service.php
+++ keyring/service.php
@@ -312,11 +312,11 @@ abstract class Keyring_Service {
 
 // Load all packaged services in the ./includes/services/ directory by including all PHP files, first in core, then in extended
 // Remove a Service (prevent it from loading at all) by filtering on 'keyring_services'
-$core_services = glob( dirname( __FILE__ ) . "/includes/services/core/*.php" );
-$core_services = is_array($core_services) ? $core_services : array();
+$core_services = glob( dirname( __FILE__ ) . '/includes/services/core/*.php' );
+$core_services = is_array( $core_services ) ? $core_services : array();
 
-$extended_services = glob( dirname( __FILE__ ) . "/includes/services/extended/*.php" );
-$extended_services = is_array($extended_services) ? $extended_services : array();
+$extended_services = glob( dirname( __FILE__ ) . '/includes/services/extended/*.php' );
+$extended_services = is_array( $extended_services ) ? $extended_services : array();
 
 $keyring_services = array_merge( $core_services, $extended_services );
 $keyring_services = apply_filters( 'keyring_services', $keyring_services );
